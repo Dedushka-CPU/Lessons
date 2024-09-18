c0b9481c4061b4f84a81b8a22457d4efbf13ea28
Andrey <zomb689@gmail.com>
2024-09-18 11:03:15 +0400

    CArteg

---
+++ KAkXotit2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <utility>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(){
+	std::pair<int,double> p={45,3.14};
+	cout<<p.first<<endl;
+	cout<<p.second<<endl; 
+	 
+	return 0;
+}
