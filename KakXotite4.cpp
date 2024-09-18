9e94fed97c2866a130267a9ddb77375cffa18989
Andrey <zomb689@gmail.com>
2024-09-18 11:11:42 +0400

    CArteg3

--- KakXotite4.cpp
+++ KakXotite4.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <utility>
+#include <string>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+enum class Color{
+		Black,
+		Red,
+		White
+};
+
+struct Point{
+		double x=0.0;
+		double y=0.0;
+		double z=0.0;
+		Color color;
+};
+
+int main(){
+	std::pair<std::string,int> p={"Hello",52};
+	auto[worg,freg]=p;
+	cout<<freg<<" "<<worg<<endl;
+	return 0;
+}
