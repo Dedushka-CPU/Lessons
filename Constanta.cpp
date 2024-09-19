b195525febe3e06794a95b790a2ac33a179607fd
Andrey <zomb689@gmail.com>
2024-09-19 10:40:30 +0400

    Constanta

---
+++ Constanta.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+
+int main(){
+	const int c1=52;
+	int x;
+	cin>>x;
+	const int c2=c1*x;
+	cout<<c2<<endl;
+	return 0;
+}
+
+
+
+
+
---
+++ Constanta2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+
+int main(){
+	const std::vector<int> v={1,2,5,94,15,52,547,8};
+	cout<<v.size()<<endl;
+	
+	return 0;
+}
+
+
+
+
+
