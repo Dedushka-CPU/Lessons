5a8f415c7b7d4de816204241665698a322d18117
_Dedushka_ <zomb689@gmail.com>
2024-09-25 11:45:04 +0400

    Add files via upload

--- 25.09/list.cpp
+++ 25.09/list.cpp
@@ -1,28 +1,57 @@
 #include <iostream>
-#include <list>
+#include <forward_list>
+#include <iterator>
 
 using std::cout;
 using std::cin;
 
 int main(){
-	std::list <int> l{55,52,85,58,5};
-	/*auto iter=l.end();
-	cout<<*iter<<"\n";
-	++iter;
+	//std::list <int> l{0,10,15,20};
+	/*auto iter=l.rbegin();
 	cout<<*iter<<"\n";
 	--iter;
 	cout<<*iter<<"\n";
-	for(size_t i=0;i!=l.size();i++){
+	++iter;
+	cout<<*iter<<"\n";*/
+	/*for(size_t i=0;i!=l.size();i++){
 		cout<<*iter<<" ";
 		++iter;	
 	}*/
-	for(auto iter=l.begin();iter!=l.end();++iter){
+	/*for(auto iter=l.begin();iter!=l.end();++iter){
 		cout<<*iter<<" ";
 	} 
 	cout<<"\n";
 	for(auto iter=l.rbegin();iter!=l.rend();++iter){
 		cout<<*iter<<" ";
+	} */
+	/*auto iter=l.begin();
+	++iter;
+	l.insert(iter,5);
+	for(auto iter=l.begin();iter!=l.end();++iter){
+		cout<<*iter<<" ";
 	} 
+	cout<<"\n";
+	for(auto iter=l.begin();iter!=l.end();++iter){
+		if(*iter%2==0){
+			iter=l.erase(iter);
+		}
+	}
+	for(auto iter=l.begin();iter!=l.end();++iter){
+		cout<<*iter<<" ";
+	} */
+	/*auto iter=std::next(l.begin());
+	cout<<*iter<<" ";
+	std::advance(iter,2);
+	cout<<*iter<<" ";
+	*/
+	std::forward_list<int> fl{0,10,15,20};
+	fl.push_front(52);
+	auto iter=std::next(fl.begin());
+	iter=fl.erase_after(iter);
+	iter=fl.insert_after(iter,4);
+	for(int x:fl){
+		cout<<x<<" ";
+	}
 	return 0;
 }
 
